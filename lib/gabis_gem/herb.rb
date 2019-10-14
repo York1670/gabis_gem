6df5d3d17fd689d3df81078a259b65e8fb6bff10
Bobbi York <york1670@gmail.com>
2019-10-14 18:49:22 +0000

    The real stuff begins, updated classes and scraping

---
+++ lib/gabis_gem/herb.rb
@@ -0,0 +1,22 @@
+ class Herb
+    attr_accessor :herb_name, :url, :medicinal_uses
+    
+    @@all = []
+    
+    def initialize(herb_name,url)
+      @herb_name = herb_name
+      @url = url
+      @@all << self
+    end
+    
+  	def self.which_herb
+  		puts "\nYou're needing information on which herb?"
+  		herb = gets.strip.downcase.to_s
+  		@@herbs._ailments.fetch("#{herb}")
+  	end
+  	
+  	def self.all
+  	  @@all
+  	end
+  	
+  end
